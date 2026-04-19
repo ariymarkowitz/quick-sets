b3770be9f415bc10964be9899f3b121dde8fe184
Ari Markowitz <ariymarkowitz@gmail.com>
2026-04-20 02:51:43 +1200

    Make modal transitions smooth

--- src/components/GameOverModal.svelte
+++ src/components/GameOverModal.svelte
@@ -1,16 +1,28 @@
 <script lang="ts">
   import { game, newGame } from '../lib/state.svelte';
   import { formatTime } from '../lib/game';
+  import { fly } from 'svelte/transition';
+  import { cubicOut } from 'svelte/easing';
 
   function playAgain() {
     game.gameOver = null;
-    newGame();
+  }
+
+  function overlayTransition(_node: Element, { duration = 350 } = {}) {
+    return {
+      duration,
+      css: (t: number) => `
+        opacity: ${t};
+        backdrop-filter: blur(${t * 5}px);
+        -webkit-backdrop-filter: blur(${t * 5}px);
+      `
+    };
   }
 </script>
 
 {#if game.gameOver}
-  <div id="overlay">
-    <div id="modal">
+  <div id="overlay" transition:overlayTransition={{ duration: 350 }}>
+    <div id="modal" transition:fly={{ y: -60, duration: 450, easing: cubicOut }} onoutroend={newGame}>
       <h2 id="modal-title">{game.gameOver.title}</h2>
       <p id="final-time-display">{formatTime(game.gameOver.time)}</p>
       <h3>Top Times</h3>
